f7f84b724f312ae21b788b8154ad9ede113b9259
Harry MacCode <emarh.harry.code@gmail.com>
2024-01-29 01:37:11 +0100

    Image Slider - CSS

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
-import './Slider.scss';
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+// import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 import { sliderData } from './slider-data';
+import './Slider.scss';
 
 
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   return (
-    <div className='slide'>
-      <FaArrowAltCircleLeft className='arrow prev' />
-      <FaArrowAltCircleRight className='arrow next' />
+    <div className='slider'>
+      <FaArrowLeft className='arrow prev' />
+      <FaArrowRight className='arrow next' />
 
       {sliderData.map((slide, index) => {
         return (
-            <div className={index === currentSlide ? 'slide current': 'slide'} key={index}>
-                {index === currentSlide && (
-                    <div>
-                        <img src={slide.image} alt="slide" />
-                        <div className="content">
-                            <h2>{slide.heading}</h2>
-                            <p>{slide.desc}</p>
-                            <hr />
-                            <button className="--btn --btn-ptimary">Get Started</button>
-                        </div>
-                    </div>
-                )}
-            </div>
-        )
+          <div className={index === currentSlide ? "slide current" : "slide"} key={index}>
+            {index === currentSlide && (
+              <>
+                <img src={slide.image} alt="slide" />
+                <div className="content">
+                  <h2>{slide.heading}</h2>
+                  <p>{slide.desc}</p>
+                  <hr />
+                  <button className="--btn --btn-primary">Get Started</button>
+                </div>
+              </>
+            )}
+          </div>
+        );
       })}
     </div>
   )
